b5b149c4b9520ab8bf3719c33261847fa34cbb45
zhangzhe03 <zhangzhe03@bilibili.com>
2025-07-01 17:48:20 +0800

    feat 2.4带有结束标记和位置标记的Tokenizer

--- src/word/splitor.py
+++ src/word/splitor.py
@@ -1,5 +1,5 @@
 # 分词器
-from simple_tokenizer_v1 import SimpleTokenizerV1
+from simple_tokenizer_v2 import SimpleTokenizerV2
 import re
 
 with open('../resource/txt/the-verdict.txt', 'r', encoding='utf-8') as f:
@@ -8,6 +8,7 @@ print("单词总数: {}".format(len(raw_text)))
 
 # 标点符号单独作为一个词
 result = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+# 对Result的元素进行strip操作，如果strip后为空，则不保留，否则保留，最终是的result是一个不包含空格的列表
 result = [item.strip() for item in result if item.strip()]
 
 print("分词总数: {}".format(len(result)))
@@ -16,14 +17,15 @@ print("分词总数: {}".format(len(result)))
 # token 转 token_ids
 #  1. 构建词汇表
 all_words = sorted(set(result))
+all_words.extend(['<|endoftext|>', '<|unk|>'])
 vocab_size = len(all_words)
 print("词汇表总数: {}".format(vocab_size))
 #  2. 构建词汇和id映射
 vocab = {token:integer for integer,token in enumerate(all_words)}
 
 # 测试编码和解码
-tokenizer = SimpleTokenizerV1(vocab)
-text = """"It's the last he painted, you know," Mrs. Gisburn said with pardonable pride."""
+tokenizer = SimpleTokenizerV2(vocab)
+text = """"Hello,It's the last he painted, you know," Mrs. Gisburn said with pardonable pride."""
 ids = tokenizer.encode(text)
 print(ids)
 strs = tokenizer.decode(ids)
